3d79d259f9264b34bb7a120f13b743d17d0650dc
Chee Lee <cheeleertr@gmail.com>
2024-07-19 11:35:45 -0500

    add more attribute test in search

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Search', type: :feature do
-  it "returns members from search by nation" do
+  it "returns members from search by nation limted to 25" do
     @members = MemberFacade.new.get_members_by_nation("fire+nation")
 
     visit "/"
@@ -12,13 +12,21 @@ RSpec.describe 'Search', type: :feature do
     expect(current_path).to eq("/search")
 
     expect(page).to have_content("Population: #{@members.count}")
-# save_and_open_page
+
     within(first(".member")) do
-      expect(page).to have_css(".name")
       expect(page).to have_css(".image")
+
+      expect(page).to have_css(".name")
+      expect(page).to have_content("Name: #{@members.first.name}")
+
       expect(page).to have_css(".allies")
+      expect(page).to have_content("Allies: #{@members.first.allies.to_sentence}")
+
       expect(page).to have_css(".affiliation")
+      expect(page).to have_content("Affiliation: #{@members.first.affiliation}")
+
       expect(page).to have_css(".enemies")
+      expect(page).to have_content("Enemies: #{@members.first.enemies.to_sentence}")
     end
   end
 end
